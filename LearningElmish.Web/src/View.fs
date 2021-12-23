59fd46b2d28515529f2a9e0501b67324b3c0fe45
Ivan Komarenko <vanyakomarenko@gmail.com>
2021-12-23 22:34:38 +0200

    added css

--- LearningElmish.Web/src/View.fs
+++ LearningElmish.Web/src/View.fs
@@ -7,43 +7,63 @@ open Feliz
 open Fulma
 
 let view (state : SumCalculator) dispatch =
-    Button.button [ Button.Color IsDark ]
-                [ str "Dark" ]
-    (*Html.div [
-        Html.h2 "First number:"
-        Html.br []
-        Html.input [
-        prop.id "first-num"
-        prop.type' "number"
-        prop.min 0
-        prop.max 50
-        prop.onChange (SetFirstNum >> dispatch)
+    Html.div [
+        Html.h2 [
+            prop.id "main-elements"
+            prop.text "First number:"
         ]
         Html.br []
-        Html.h2 "Second number:"
-        Html.br []
-        Html.input [
-            prop.id "second-num"
-            prop.type' "number"
-            prop.min 0
-            prop.max 50
-            prop.onChange (SetSecondNum >> dispatch)
-        ]
-        Html.br []
-        Html.button [
-            prop.onClick (fun _ -> dispatch Count)
-            prop.text "Sum"
-            
-        ]
-        Html.br []
-        Html.h2 "Result:"
-        Html.h1 state.Result
         Html.div [
-            prop.id "main"
-            prop.className "shiny"
+            prop.id "main-elements"
             prop.children [
-                Html.text "Дюдя, хало)))"
+                Html.input [
+                    prop.id "main-elements"
+                    prop.type' "number"
+                    prop.min 0
+                    prop.max 50
+                    prop.onChange (SetFirstNum >> dispatch)
+                ]
             ]
         ]
-    ]*)
-    
+        
+        Html.br []
+        Html.h2 [
+            prop.id "main-elements"
+            prop.text "Second number:"
+        ]
+        Html.br []
+        Html.div [
+            prop.id "main-elements"
+            prop.children [
+                Html.input [
+                    prop.id "main-elements"
+                    prop.type' "number"
+                    prop.min 0
+                    prop.max 50
+                    prop.onChange (SetSecondNum >> dispatch)
+                ]
+            ]
+        ]
+        
+        Html.br []
+        Html.div [
+            prop.id "main-elements"
+            prop.children [
+                Html.button [
+                    prop.id "main-elements"
+                    prop.onClick (fun _ -> dispatch Count)
+                    prop.text "Sum"
+                ]
+            ]
+        ]
+        
+        Html.br []
+        Html.h2 [
+            prop.id "main-elements"
+            prop.text ("Result: " + string state.Result)
+        ]
+        Html.h1 [
+            prop.id "info-text"
+            prop.text "Простий додаток для знаходження суми чисел."
+        ]
+    ]
--- LearningElmish.Web/src/View.fs.js
+++ LearningElmish.Web/src/View.fs.js
@@ -1,8 +1,62 @@
-import { Option, button } from "./.fable/Fulma.2.3.0/Elements/Button.fs.js";
-import { Color_IColor } from "./.fable/Fulma.2.3.0/Common.fs.js";
-import { singleton } from "./.fable/fable-library.3.2.9/List.js";
+import { createElement } from "react";
+import { Interop_reactApi } from "./.fable/Feliz.1.52.0/Interop.fs.js";
+import { int32ToString, round } from "./.fable/fable-library.3.2.9/Util.js";
+import { Message } from "./Types.fs.js";
+import { ofArray } from "./.fable/fable-library.3.2.9/List.js";
 
 export function view(state, dispatch) {
-    return button(singleton(new Option(0, new Color_IColor(1))), singleton("Dark"));
+    const children = ofArray([createElement("h2", {
+        id: "main-elements",
+        children: "First number:",
+    }), createElement("br", {}), createElement("div", {
+        id: "main-elements",
+        children: Interop_reactApi.Children.toArray([createElement("input", {
+            id: "main-elements",
+            type: "number",
+            min: 0,
+            max: 50,
+            onChange: (ev) => {
+                const value_14 = ev.target.valueAsNumber;
+                if ((!(value_14 == null)) ? (value_14 !== NaN) : false) {
+                    dispatch(new Message(0, round(value_14)));
+                }
+            },
+        })]),
+    }), createElement("br", {}), createElement("h2", {
+        id: "main-elements",
+        children: "Second number:",
+    }), createElement("br", {}), createElement("div", {
+        id: "main-elements",
+        children: Interop_reactApi.Children.toArray([createElement("input", {
+            id: "main-elements",
+            type: "number",
+            min: 0,
+            max: 50,
+            onChange: (ev_1) => {
+                const value_31 = ev_1.target.valueAsNumber;
+                if ((!(value_31 == null)) ? (value_31 !== NaN) : false) {
+                    dispatch(new Message(1, round(value_31)));
+                }
+            },
+        })]),
+    }), createElement("br", {}), createElement("div", {
+        id: "main-elements",
+        children: Interop_reactApi.Children.toArray([createElement("button", {
+            id: "main-elements",
+            onClick: (_arg1) => {
+                dispatch(new Message(2));
+            },
+            children: "Sum",
+        })]),
+    }), createElement("br", {}), createElement("h2", {
+        id: "main-elements",
+        children: "Result: " + int32ToString(state.Result),
+    }), createElement("h1", {
+        id: "info-text",
+        children: "Простий додаток для знаходження суми чисел.",
+    })]);
+    return createElement("div", {
+        children: Interop_reactApi.Children.toArray(Array.from(children)),
+    });
 }
 
